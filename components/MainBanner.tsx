27faf7a726b700870c6cfe6d36508b1d98c21c3c
Vatsal Sejpal <sejpalvatsal456@gmail.com>
2025-12-08 13:19:12 +0530

    Added frontend of main banner

--- components/MainBanner.tsx
+++ components/MainBanner.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+'use client';
+
+import React from "react";
+import Image from "next/image";
 
 export default function MainBanner() {
   return (
-    <div id='main-banner'>
-        <span>MainBanner</span>
+    <div id="main-banner">
+      <div id="text-section">
+        <div id="temp-container">
+          <h2>I'm just another...</h2>
+        <h1>Web Developer</h1>
+        <button type="button" onClick={(e) => (location.href = "#contactme")}>
+          My Resume
+        </button>
+        </div>
+      </div>
+      <div id="image-section">
+        <Image
+          src="/img/web.png"
+          alt="#"
+          width={500}
+          height={370}
+        />
+      </div>
     </div>
-  )
+  );
 }
